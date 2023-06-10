52685237664b5708a4d7e466c8a597343fe620b9
Kunal <33kunalg@gmail.com>
2023-06-11 00:09:18 +0530

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import authRoutes from "./Routes/auths.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -22,7 +23,7 @@ const connect = () => {
       throw err;
     });
 };
-
+app.use(cors())
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/users", userRoutes);
